19b918eebd9c977984e631519b6ffa13bdf23f60
Yuva Raju <98yuvaraju@gmail.com>
2018-03-01 23:32:03 +0530

    Do necessary changes so as it wrks in python2.7

--- myproc/try2.py
+++ myproc/try2.py
@@ -11,8 +11,27 @@ def echo():
     email= request.form['email']
     sub= request.form['sub']
     dis= request.form['dis']
-    return (name + email +sub +dis)
-     
-     
+    return valid(name,email,sub,dis)
+
+@app.route("/echo2", methods=['POST'])
+def echo2(): 
+    item= request.form['id']
+    return item
+
+def valid(name,email,sub,dis):
+    if '@' in email and all(x.isalpha() or x.isspace() for x in name) and sub and dis :
+      
+        return render_template("success.html")
+
+    else:
+        return render_template("fail.html")
+
+    
+    
+    
+    
+    
+
+   
 if __name__ == "__main__":
     app.run()
